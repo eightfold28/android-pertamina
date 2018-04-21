40aa00b54e760ac75d08f54b927dfb143faaff37
Dharma Kurnia Septialoka <13514028@std.stei.itb.ac.id>
2018-04-21 18:13:55 +0700

    awal page

---
+++ app/src/main/java/com/example/septialoka/p_predictor/sign_in.java
@@ -0,0 +1,23 @@
+package com.example.septialoka.p_predictor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class sign_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        findViewById(R.id.text_sign_in).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(sign_in.this, navigation_menu.class);
+                startActivity(i);
+            }
+        });
+    }
+}
